ee59b03268acc75e13ca40b065da63427524d34c
Ralph Woiwode <rawoiwode@gmail.com>
2026-01-29 11:34:14 -0600

    Hitting PokeAPI to retrieve simple data to display Pokemon for the Pokedex

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,60 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
+interface Pokemon {
+  name: string;
+  id: number;
+  image: string;
+}
 export default function Index() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  async function fetchPokemon() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20",
+      );
+      const data = await response.json();
+
+      // Fetch detailed info for each Pokemon in parallel
+      const detailedPokemon = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+
+          return {
+            name: pokemon.name,
+            id: pokemon.id,
+            image: details.sprites.front_default, // main sprite
+          };
+        }),
+      );
+
+      // console.log(detailedPokemon);
+
+      // console.log(data);
+      setPokemon(detailedPokemon);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    // Fetch Pokemon
+    fetchPokemon();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      {pokemon.map((pokemon) => (
+        <View key={pokemon.id}>
+          <Text>{pokemon.name}</Text>
+          <Image
+            source={{ uri: pokemon.image }}
+            style={{ width: 100, height: 100 }}
+          />
+        </View>
+      ))}
+    </ScrollView>
   );
 }
