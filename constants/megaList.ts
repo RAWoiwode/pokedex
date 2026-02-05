6113b3fc215f9892f66325e065a9c495a9525b19
Ralph Woiwode <rawoiwode@gmail.com>
2026-02-05 15:34:03 -0600

    Creating list of all mega pokmeon to make it easier to fetch mega sprites

---
+++ constants/megaList.ts
@@ -0,0 +1,96 @@
+const MEGA_POKEMON = new Map([
+  // X & Y
+  ["venusaur", 1],
+  ["charizard", 2], // 'mega-x' & 'mega-y'
+  ["blastoise", 1],
+  ["alakazam", 1],
+  ["gengar", 1],
+  ["kangaskhan", 1],
+  ["pinsir", 1],
+  ["gyarados", 1],
+  ["aerodactyl", 1],
+  ["mewtwo", 2], // 'mega-x' & 'mega-y'
+  ["ampharos", 1],
+  ["scizor", 1],
+  ["heracross", 1],
+  ["houndoom", 1],
+  ["tyranitar", 1],
+  ["blaziken", 1],
+  ["gardevoir", 1],
+  ["mawile", 1],
+  ["aggron", 1],
+  ["medicham", 1],
+  ["manectric", 1],
+  ["banette", 1],
+  ["absol", 2], // 'mega' & 'mega-z'
+  ["latias", 1],
+  ["latios", 1],
+  ["garchomp", 2], // 'mega' & 'mega-z'
+  ["lucario", 2], // 'mega' & 'mega-z'
+  ["abomasnow", 1],
+
+  // Omega Ruby & Alpha Sapphire
+  ["beedrill", 1],
+  ["pidgeot", 1],
+  ["slowbro", 1],
+  ["steelix", 1],
+  ["sceptile", 1],
+  ["swampert", 1],
+  ["sableye", 1],
+  ["sharpedo", 1],
+  ["camerupt", 1],
+  ["altaria", 1],
+  ["glalie", 1],
+  ["salamence", 1],
+  ["metagross", 1],
+  ["rayquaza", 1],
+  ["lopunny", 1],
+  ["gallade", 1],
+  ["audino", 1],
+  ["diancie", 1],
+
+  // Legends: Z-A (new megas)
+  ["clefable", 1],
+  ["victreebel", 1],
+  ["starmie", 1],
+  ["dragonite", 1],
+  ["meganium", 1],
+  ["feraligatr", 1],
+  ["skarmory", 1],
+  ["froslass", 1],
+  ["emboar", 1],
+  ["excadrill", 1],
+  ["scolipede", 1],
+  ["scrafty", 1],
+  ["eelektross", 1],
+  ["chandelure", 1],
+  ["chesnaught", 1],
+  ["delphox", 1],
+  ["greninja", 1],
+  ["pyroar", 1],
+  ["floette-eternal", 1], // 'floette-mega'
+  ["malamar", 1],
+  ["barbaracle", 1],
+  ["dragalge", 1],
+  ["hawlucha", 1],
+  ["zygarde-complete", 1], // 'zygarde-mega'
+  ["drampa", 1],
+  ["falinks", 1],
+
+  // Pokemon Legends Z-A: Mega Dimension (DLC)
+  ["raichu", 2], // 'mega-x' & 'mega-y'
+  ["chimecho", 1],
+  ["staraptor", 1],
+  ["heatran", 1],
+  ["darkrai", 1],
+  ["golurk", 1],
+  ["meowstic", 1],
+  ["crabominable", 1],
+  ["golisopod", 1],
+  ["magearna", 2], // 'mega', 'original-mega'
+  ["zeraora", 1],
+  ["scovillain", 1],
+  ["glimmora", 1],
+  ["tatsugiri", 3], // 'tatsugiri-curly-mega', 'tatsugiri-droopy-mega', 'tatsugiri-stretchy-mega'
+  ["baxcalibur", 1],
+]);
